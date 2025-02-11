8a420dc45ac7d67ed0efb99e6cce3661654a1fe6
Yannmgh <yannmgh@gmail.com>
2025-02-11 15:33:55 +0100

    Nouveau exo

--- Main3.java
+++ Main3.java
@@ -1,26 +1,19 @@
-import java.util.Scanner;
-
 public class Main3 {
-    public static void lireValeurTableau(int[] tableau, int index) {
-        try {
-            int valeur = tableau[index];  // Tentative d'accès à l'index donné
-            System.out.println("Valeur à l'index " + index + " : " + valeur);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Erreur : L'index " + index + " est hors limites ! Taille du tableau : " + tableau.length);
-        } catch (Exception e) {
-            System.out.println("Une erreur inattendue s'est produite : " + e.getMessage());
+    static String[] cours = {"Dev JAva", "Management", "Cyber"};
+
+    public static String getElement(int index) throws ArrayIndexOutOfBoundsException {
+        if (cours.length < index) {
+            throw new ArrayIndexOutOfBoundsException("Vous cherchez un element qui est hors du tableau !");
         }
+
+        return cours[index];
     }
-
+    
     public static void main(String[] args) {
-        int[] tableau = {10, 20, 30, 40, 50};
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Entrez un index : ");
-        int index = scanner.nextInt();
-
-        lireValeurTableau(tableau, index);
-
-        scanner.close();
+        try {
+            System.out.println(getElement(6));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erreur capturée : " + e.getMessage());
+        }
     }
 }
